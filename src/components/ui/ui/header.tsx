aad694b140fcec9162ce2930084d940dcc4d1417
SpeedyxBR <andrel.cilva57@gmail.com>
2025-11-03 06:16:59 -0300

    feat: add header menu using sheet components

--- src/components/ui/ui/header.tsx
+++ src/components/ui/ui/header.tsx
@@ -1,17 +1,54 @@
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
+import { ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, HomeIcon} from "lucide-react";
 import { ShoppingCartIcon } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 
 const Header = () => {
   return (
     <Card className="flex justify-between items-center p-8 align-items-center">
-      <Button size= "icon">
+     <Sheet >
+        <SheetTrigger asChild>
+     <Button size= "icon">
         <MenuIcon />
       </Button>
+      </SheetTrigger> 
+
+      <SheetContent side="left">
+        <SheetHeader className="text-left">
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+
+        <div className="mt-2 flex flex-col gap-2">
+          <Button className="w-full justify-start gap-2">
+            <LogInIcon size={16} />
+            Fazer Login
+          </Button>
+
+          <Button className="w-full justify-start gap-2">
+            <HomeIcon size={16}  />
+            Início
+          </Button>
+
+          <Button className="w-full justify-start gap-2">
+            <PercentIcon size={16}  />
+            Ofertas
+          </Button>
+
+          <Button className="w-full justify-start gap-2">
+            <ListOrderedIcon size={16}  />
+            Catálogo 
+          </Button>
+        
+        </div>
+
+      </SheetContent>
+      </Sheet>
+
+      
 
       <h1 className="text-lg font-semibold">
-        <span className="text-primary">E-COMMERCE</span> Plastico
+        <span className="text-primary">ECO</span> Plástico
       </h1>
       <Button size= "icon">
         <ShoppingCartIcon />
